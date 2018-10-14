a0fc84349f26148c621ee23b653cc7ff5a3d1dc2
Yujun Liu <dreamzack@gmail.com>
2018-10-14 16:57:43 -0700

    finished part video

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,24 +1,42 @@
 import * as React from "react";
 import { Form, Icon, Input, Button } from "antd";
+import { withFormik, FormikErrors, FormikProps } from "formik";
 
 const FormItem = Form.Item;
 
-export class RegisterView extends React.PureComponent {
+interface FormValues {
+  email: string;
+  password: string;
+}
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
+
+class C extends React.PureComponent<FormikProps<FormValues> & Props> {
   render() {
+    const { values, handleChange, handleBlur, handleSubmit } = this.props;
     return (
-      <div style={{ display: "flex" }}>
+      <form style={{ display: "flex" }} onSubmit={handleSubmit}>
         <div style={{ width: 400, margin: "auto" }}>
           <FormItem>
             <Input
+              name="email"
               prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-              placeholder="Username"
+              placeholder="Email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
             />
           </FormItem>
           <FormItem>
             <Input
+              name="password"
               prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
               type="password"
               placeholder="Password"
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
             />
           </FormItem>
           <FormItem>
@@ -39,7 +57,17 @@ export class RegisterView extends React.PureComponent {
             Or <a href="">Log in now!</a>
           </FormItem>
         </div>
-      </div>
+      </form>
     );
   }
 }
+
+export const RegisterView = withFormik<Props, FormValues>({
+  mapPropsToValues: () => ({ email: "", password: "" }),
+  handleSubmit: async (values, { props, setErrors }) => {
+    const errors = await props.submit(values);
+    if (errors) {
+      setErrors(errors);
+    }
+  }
+})(C);
